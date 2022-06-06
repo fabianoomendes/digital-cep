a3f74c5b873b08bc566cdaa702cd470ffed08273
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-06-06 05:26:34 -0300

    Update

--- src/Search.php
+++ src/Search.php
@@ -1,15 +1,31 @@
-<?php 
+<?php
 
 namespace FabianoMendes\DigitalCep;
 
-class Search{
-    private $url = "https://viacep.com.br/ws/";
+use FabianoMendes\DigitalCep\ws\ViaCep;
 
-    public function getAddressFromZipcode(String $zipCode): array {
+class Search
+{
+    public function getAddressFromZipcode(string $zipCode): array
+    {
         $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
 
-        $get = file_get_contents($this->url . $zipCode . "/json");
+        return $this->getFromServer($zipCode);
+    }
 
-        return (array) json_decode($get);
+    private function getFromServer(string $zipCode): array
+    {
+        $get = new ViaCep();
+
+        return $get->get($zipCode);
+    }
+
+    private function processData($data)
+    {
+        foreach ($data as $k => $v) {
+            $data[$k] = trim($v);
+        }
+
+        return $data;
     }
 }
